8fbd2c25c21bfedf8e9d4cd14c2322292ed32330
devjean <jeancarloleall@gmail.com>
2023-07-16 16:17:35 -0300

    making grid in main

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,4 @@
-import { Home as HomeIcon, Search, Library } from 'lucide-react'
+import { Home as HomeIcon, Search, Library, ChevronLeft, ChevronRight } from 'lucide-react'
 
 export default function Home() {
   return (
@@ -25,9 +25,34 @@ export default function Home() {
               Your Library
             </a>
           </nav>
+
+          <nav className='mt-6 pt-6 border-t border-zinc-800 flex flex-col gap-3'>
+            <a className='text-sm text-zinc-400 hover:text-zinc-100' href="">Hot Hits Brasil</a>
+            <a className='text-sm text-zinc-400 hover:text-zinc-100' href="">Aniver Funk</a>
+            <a className='text-sm text-zinc-400 hover:text-zinc-100' href="">My Playlist #13</a>
+            <a className='text-sm text-zinc-400 hover:text-zinc-100' href="">Top Brasil</a>
+          </nav>
         </aside>
         <main className="flex-1 p-6">
-          main
+          <div className='flex items-center gap-4'>
+            <button className='rounded-full bg-black/40 p-1'>
+              <ChevronLeft />
+            </button>
+            <button className='rounded-full bg-black/40 p-1'>
+              <ChevronRight />
+            </button>
+          </div>
+
+          <h1 className='font-semibold text-3xl mt-10'>Good Afternoon</h1>
+
+          <div className='grid grid-cols-3 gap-6'>
+            <div className='bg-white/10'>Playlist</div>
+            <div className='bg-white/10'>Playlist</div>
+            <div className='bg-white/10'>Playlist</div>
+            <div className='bg-white/10'>Playlist</div>
+            <div className='bg-white/10'>Playlist</div>
+            <div className='bg-white/10'>Playlist</div>
+          </div>
         </main>
       </div>
       <footer className="bg-zinc-800 border-t border-zinc-700 p-6">
