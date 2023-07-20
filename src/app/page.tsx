c1908385dd924c133813c73ec13a666af9361003
devjean <jeancarloleall@gmail.com>
2023-07-20 01:59:16 -0300

    made for album

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,4 @@
-import { Home as HomeIcon, Search, Library, ChevronLeft, ChevronRight } from 'lucide-react'
+import { Home as HomeIcon, Search, Library, ChevronLeft, ChevronRight, Play } from 'lucide-react'
 
 import Image from 'next/image';
 
@@ -47,24 +47,74 @@ export default function Home() {
           <h1 className='font-semibold text-3xl mt-10'>Good Afternoon</h1>
 
           <div className='grid grid-cols-3 gap-4 mt-4'>
-            <div className='bg-white/10 rounded overflow-hidden'>
-              <Image src="/album.jpg" width={120} height={120} alt="album Foo Fighters" />
-            </div>
-            <div className='bg-white/10 rounded overflow-hidden'>
-              <Image src="/album.jpg" width={120} height={120} alt="album Foo Fighters" />
-            </div>
-            <div className='bg-white/10 rounded overflow-hidden'>
-              <Image src="/album.jpg" width={120} height={120} alt="album Foo Fighters" />
-            </div>
-            <div className='bg-white/10 rounded overflow-hidden'>
-              <Image src="/album.jpg" width={120} height={120} alt="album Foo Fighters" />
-            </div>
-            <div className='bg-white/10 rounded overflow-hidden'>
-              <Image src="/album.jpg" width={120} height={120} alt="album Foo Fighters" />
-            </div>
-            <div className='bg-white/10 rounded overflow-hidden'>
-              <Image src="/album.jpg" width={120} height={120} alt="album Foo Fighters" />
-            </div>
+            <a href="#" className='bg-white/5 group rounded flex items-center gap-4 overflow-hidden hover:bg-white/10 transition-colors'>
+              <Image src="/album.jpg" width={104} height={104} alt="Capa álbum Wasting Light da banda Foo Fighters" />
+              <strong>Wasting Light</strong>
+
+              <button className='w-12 h-12 flex items-center justify-center pl-1 rounded-full bg-green-400 text-black ml-auto mr-8 invisible group-hover:visible'>
+                <Play />
+              </button>
+            </a>
+            <a href="#" className='bg-white/5 group rounded flex items-center gap-4 overflow-hidden hover:bg-white/10 transition-colors'>
+              <Image src="/album.jpg" width={104} height={104} alt="Capa álbum Wasting Light da banda Foo Fighters" />
+              <strong>Wasting Light</strong>
+
+              <button className='w-12 h-12 flex items-center justify-center pl-1 rounded-full bg-green-400 text-black ml-auto mr-8 invisible group-hover:visible'>
+                <Play />
+              </button>
+            </a>
+            <a href="#" className='bg-white/5 group rounded flex items-center gap-4 overflow-hidden hover:bg-white/10 transition-colors'>
+              <Image src="/album.jpg" width={104} height={104} alt="Capa álbum Wasting Light da banda Foo Fighters" />
+              <strong>Wasting Light</strong>
+
+              <button className='w-12 h-12 flex items-center justify-center pl-1 rounded-full bg-green-400 text-black ml-auto mr-8 invisible group-hover:visible'>
+                <Play />
+              </button>
+            </a>
+            <a href="#" className='bg-white/5 group rounded flex items-center gap-4 overflow-hidden hover:bg-white/10 transition-colors'>
+              <Image src="/album.jpg" width={104} height={104} alt="Capa álbum Wasting Light da banda Foo Fighters" />
+              <strong>Wasting Light</strong>
+
+              <button className='w-12 h-12 flex items-center justify-center pl-1 rounded-full bg-green-400 text-black ml-auto mr-8 invisible group-hover:visible'>
+                <Play />
+              </button>
+            </a>
+            <a href="#" className='bg-white/5 group rounded flex items-center gap-4 overflow-hidden hover:bg-white/10 transition-colors'>
+              <Image src="/album.jpg" width={104} height={104} alt="Capa álbum Wasting Light da banda Foo Fighters" />
+              <strong>Wasting Light</strong>
+
+              <button className='w-12 h-12 flex items-center justify-center pl-1 rounded-full bg-green-400 text-black ml-auto mr-8 invisible group-hover:visible'>
+                <Play />
+              </button>
+            </a>
+            <a href="#" className='bg-white/5 group rounded flex items-center gap-4 overflow-hidden hover:bg-white/10 transition-colors'>
+              <Image src="/album.jpg" width={104} height={104} alt="Capa álbum Wasting Light da banda Foo Fighters" />
+              <strong>Wasting Light</strong>
+
+              <button className='w-12 h-12 flex items-center justify-center pl-1 rounded-full bg-green-400 text-black ml-auto mr-8 invisible group-hover:visible'>
+                <Play />
+              </button>
+            </a>
+          </div>
+
+          <h2 className='font-semibold text-2xl mt-10'>Made for Jean Carlo</h2>
+
+          <div className='grid grid-cols-6 gap-4 mt-4'>
+            <a href="" className='bg-white/5 p-3 rounded hover:bg-white/10'>
+            <Image src="/album.jpg" className='w-full' width={120} height={120} alt="Capa álbum Wasting Light da banda Foo Fighters" />
+            </a>
+            <a href="" className='bg-white/5 p-3 rounded hover:bg-white/10'>
+            <Image src="/album.jpg" className='w-full' width={120} height={120} alt="Capa álbum Wasting Light da banda Foo Fighters" />
+            </a>
+            <a href="" className='bg-white/5 p-3 rounded hover:bg-white/10'>
+            <Image src="/album.jpg" className='w-full' width={120} height={120} alt="Capa álbum Wasting Light da banda Foo Fighters" />
+            </a>
+            <a href="" className='bg-white/5 p-3 rounded hover:bg-white/10'>
+            <Image src="/album.jpg" className='w-full' width={120} height={120} alt="Capa álbum Wasting Light da banda Foo Fighters" />
+            </a>
+            <a href="" className='bg-white/5 p-3 rounded hover:bg-white/10'>
+            <Image src="/album.jpg" className='w-full' width={120} height={120} alt="Capa álbum Wasting Light da banda Foo Fighters" />
+            </a>
           </div>
         </main>
       </div>
